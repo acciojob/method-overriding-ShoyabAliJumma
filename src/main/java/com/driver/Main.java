d22d4bf28836e1df3b87e00b26a610da07d99a3e
ShoyabAliJumma <shoyabalipathan@gmail.com>
2023-01-13 17:36:04 +0530

    solution 6

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,7 +16,7 @@ public class Main
   {
         public  String meth()
         {
-            return "Method is overridden in Extended class B";
+            return "Method is overridden in Extendend class B";
         }
   }
 }
